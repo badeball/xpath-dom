17ac78ddf3e7460da0f4da320909a7432a4cc441
Jonas Amundsen <jonasba@gmail.com>
2015-09-25 15:55:44 +0200

    Add browser configurations for Sauce Labs

--- karma.conf.js
+++ karma.conf.js
@@ -28,6 +28,60 @@ module.exports = function(config) {
 
     logLevel: config.LOG_INFO,
 
+    customLaunchers: {
+      Firefox: {
+        base: "SauceLabs",
+        browserName: "Firefox",
+        platform: "Windows 7"
+      },
+
+      Chrome: {
+        base: "SauceLabs",
+        browserName: "Chrome",
+        platform: "Windows 7"
+      },
+
+      Opera: {
+        base: "SauceLabs",
+        browserName: "Opera",
+        platform: "Windows 7"
+      },
+
+      Safari: {
+        base: "SauceLabs",
+        browserName: "Safari",
+        platform: "OS X 10.11"
+      },
+
+      IE11: {
+        base: "SauceLabs",
+        browserName: "Internet Explorer",
+        version: "11",
+        platform: "Windows 7"
+      },
+
+      IE10: {
+        base: "SauceLabs",
+        browserName: "Internet Explorer",
+        version: "10",
+        platform: "Windows 7"
+      },
+
+      IE9: {
+        base: "SauceLabs",
+        browserName: "Internet Explorer",
+        version: "9",
+        platform: "Windows 7"
+      },
+
+      IE8: {
+        base: "SauceLabs",
+        browserName: "Internet Explorer",
+        version: "8",
+        platform: "Windows 7"
+      }
+    },
+
     browsers: ["PhantomJS"],
 
     singleRun: true
