b36140468b2535bf4c0383d63fc06c6d2c65f258
Jonas Amundsen <jonasba@gmail.com>
2015-10-26 11:43:13 +0100

    Add a very basic performance test
    This consists of a server and a client application that will performing
    testing using benchmark.js. To run it, execute the following command
    
      $ node test/perf/server.js
    
    and navigate to http://localhost:8080 in your favourite browser. The
    implementation is currently compared against the native implementation
    and Google's wgxpath.
    
    The preliminary results show that my implementation is somewhat slower
    than Google's.
    
      document#evaluate x 250,487 ops/sec ±0.93% (65 runs sampled)
      XPathDOM#evaluate x 24,319 ops/sec ±3.34% (67 runs sampled)
      wgxpath#evaluate x 77,202 ops/sec ±1.97% (61 runs sampled)
      Fastest is document#evaluate

---
+++ test/perf/application.js
@@ -0,0 +1,43 @@
+/* eslint-env browser, node */
+
+"use strict";
+
+var Benchmark = require("benchmark");
+
+var XPathDOM = require("../../register");
+
+var wgxpath = require("wgxpath");
+
+var Suite = new Benchmark.Suite();
+
+var expression = ".//p[contains(., 'Hello world!')]";
+
+var mockWindow = {
+  document: {}
+};
+
+wgxpath.install(mockWindow);
+
+Suite.
+
+add("document#evaluate", function () {
+  document.evaluate(expression, document.body, null, XPathResult.FIRST_ORDERED_NODE_TYPE);
+}).
+
+add("XPathDOM#evaluate", function () {
+  XPathDOM.evaluate(expression, document.body, null, XPathDOM.XPathResult.FIRST_ORDERED_NODE_TYPE);
+}).
+
+add("wgxpath#evaluate", function () {
+  mockWindow.document.evaluate(expression, document.body, null, mockWindow.XPathResult.FIRST_ORDERED_NODE_TYPE);
+}).
+
+on("cycle", function(event) {
+  document.write(String(event.target) + "\n");
+}).
+
+on("complete", function() {
+  document.write("Fastest is " + this.filter("fastest").pluck("name") + "\n");
+}).
+
+run({async: true});
