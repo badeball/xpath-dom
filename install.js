e4d825e5d76d40f8030c7e4fe73245356d44eb1d
Jonas Amundsen <jonasba@gmail.com>
2018-04-22 18:17:27 +0200

    Remove redundant assignment of result types
    This is already done by xpath-evaluator.

--- install.js
+++ install.js
@@ -5,12 +5,4 @@ if (!window.document.evaluate) {
   window.document.createExpression = XPathEvaluator.createExpression;
   window.document.createNSResolver = XPathEvaluator.createNSResolver;
   window.XPathResult = XPathEvaluator.XPathResult;
-
-  var property;
-
-  for (property in XPathEvaluator.XPathResultType) {
-    if (XPathEvaluator.XPathResultType.hasOwnProperty(property)) {
-      window.XPathResult[property] = XPathEvaluator.XPathResultType[property];
-    }
-  }
 }
