4c505455aa269c19e3404adf7fc6f5e0f0cb87d3
Jonas Amundsen <jonasba@gmail.com>
2019-01-05 15:56:31 +0100

    Remove everything related to babel
    I'm not entirely sure why Babel was there in the first place. By using
    babel, we polyfilled things we shouldn't have and thus masking some
    weaknesses that would have prevented this from running in EG. IE9.

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function(config) {
     ],
 
     preprocessors: {
-      "test/unit/**/*_test.js": ["rollup", "babel"]
+      "test/unit/**/*_test.js": ["rollup"]
     },
 
     rollupPreprocessor: {
