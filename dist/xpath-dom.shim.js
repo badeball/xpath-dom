72382836c858d884d0582b6041219935c5b41cd7
Jonas Amundsen <jonasba@gmail.com>
2019-02-11 14:45:00 +0100

    Convert everything to using classes
    I have previously avoided doing this in order to target ES5. However,
    with the upcoming introduction of TypeScript, this will be handled by
    the compiler.

--- dist/xpath-dom.shim.js
+++ dist/xpath-dom.shim.js
@@ -2238,157 +2238,159 @@
     IMPLEMENTATION_SPECIFIC: 32
   };
 
-  function XPathDOM (nativeNode) {
-    if (!nativeNode) {
-      throw new Error("This should not happen!");
-    }
-
-    this.nativeNode = nativeNode;
-  }
-
-  XPathDOM.prototype.getNativeNode = function () {
-    return this.nativeNode;
-  };
-
-  XPathDOM.prototype.asString = function () {
-    if (typeof this.nativeNode.textContent === "string") {
-      return this.nativeNode.textContent;
-    } else {
-      var text = "";
-
-      var children = this.getChildNodes();
-
-      for (var i = 0; i < children.length; i++) {
-        text = text + children[i].asString();
+  class XPathDOM {
+    constructor(nativeNode) {
+      if (!nativeNode) {
+        throw new Error("This should not happen!");
       }
 
-      return text;
-    }
-  };
-
-  XPathDOM.prototype.asNumber = function () {
-    return +this.asString();
-  };
-
-  XPathDOM.prototype.getNodeType = function () {
-    return this.nativeNode.nodeType;
-  };
-
-  XPathDOM.prototype.getChildNodes = function () {
-    var children = [];
-
-    for (var i = 0; i < this.nativeNode.childNodes.length; i++) {
-      children.push(new XPathDOM(this.nativeNode.childNodes[i]));
+      this.nativeNode = nativeNode;
     }
 
-    return children;
-  };
-
-  XPathDOM.prototype.getFollowingSiblings = function () {
-    var node = this.nativeNode, followingSiblings = [];
-
-    while ((node = node.nextSibling)) {
-      followingSiblings.push(new XPathDOM(node));
+    getNativeNode() {
+      return this.nativeNode;
     }
 
-    return followingSiblings;
-  };
+    asString() {
+      if (typeof this.nativeNode.textContent === "string") {
+        return this.nativeNode.textContent;
+      } else {
+        var text = "";
 
-  XPathDOM.prototype.getPrecedingSiblings = function () {
-    var node = this.nativeNode, precedingSiblings = [];
+        var children = this.getChildNodes();
 
-    while ((node = node.previousSibling)) {
-      precedingSiblings.unshift(new XPathDOM(node));
+        for (var i = 0; i < children.length; i++) {
+          text = text + children[i].asString();
+        }
+
+        return text;
+      }
     }
 
-    return precedingSiblings;
-  };
-
-  XPathDOM.prototype.getName = function () {
-    var name = this.nativeNode.tagName || this.nativeNode.name;
-
-    if (name) {
-      return name.toLowerCase();
-    }
-  };
-
-  XPathDOM.prototype.getAttributes = function () {
-    if (!this.nativeNode.attributes) {
-      return [];
+    asNumber() {
+      return +this.asString();
     }
 
-    var attributes = [];
-
-    for (var i = 0; i < this.nativeNode.attributes.length; i++) {
-      attributes.push(new XPathDOM(this.nativeNode.attributes[i]));
+    getNodeType() {
+      return this.nativeNode.nodeType;
     }
 
-    return attributes;
-  };
+    getChildNodes() {
+      var children = [];
 
-  XPathDOM.prototype.getParent = function () {
-    return new XPathDOM(this.nativeNode.parentNode || this.nativeNode.ownerElement);
-  };
+      for (var i = 0; i < this.nativeNode.childNodes.length; i++) {
+        children.push(new XPathDOM(this.nativeNode.childNodes[i]));
+      }
 
-  XPathDOM.prototype.getOwnerDocument = function () {
-    if (this.nativeNode.ownerDocument) {
-      return new XPathDOM(this.nativeNode.ownerDocument);
-    }
-  };
-
-  XPathDOM.prototype.getElementById = function (id) {
-    var node = this.nativeNode.getElementById(id);
-
-    if (node) {
-      return new XPathDOM(node);
-    }
-  };
-
-  XPathDOM.prototype.isEqual = function (node) {
-    return this.getNativeNode() === node.getNativeNode();
-  };
-
-  XPathDOM.prototype.compareDocumentPosition = function (other) {
-    var self = this;
-
-    if (self.getNativeNode() instanceof Attr) {
-      self = self.getParent();
+      return children;
     }
 
-    if (other.getNativeNode() instanceof Attr) {
-      other = other.getParent();
+    getFollowingSiblings() {
+      var node = this.nativeNode, followingSiblings = [];
+
+      while ((node = node.nextSibling)) {
+        followingSiblings.push(new XPathDOM(node));
+      }
+
+      return followingSiblings;
     }
 
-    var comparing = self.getNativeNode().compareDocumentPosition(other.getNativeNode());
+    getPrecedingSiblings() {
+      var node = this.nativeNode, precedingSiblings = [];
 
-    if (comparing & DocumentPosition.PRECEDING) {
-      return 1;
-    } else if (comparing & DocumentPosition.FOLLOWING) {
-      return -1;
-    } else {
-      return 0;
-    }
-  };
+      while ((node = node.previousSibling)) {
+        precedingSiblings.unshift(new XPathDOM(node));
+      }
 
-  XPathDOM.prototype.toString = function () {
-    var name;
-
-    if (this.nativeNode.tagName) {
-      name = this.nativeNode.tagName.toLowerCase();
-    } else {
-      name = this.nativeNode.nodeName + "(" + this.nativeNode.nodeValue + ")";
+      return precedingSiblings;
     }
 
-    if (this.nativeNode.className) {
-      name = name + "." + this.nativeNode.className.split(/\s+/g).join(".");
+    getName() {
+      var name = this.nativeNode.tagName || this.nativeNode.name;
+
+      if (name) {
+        return name.toLowerCase();
+      }
     }
 
-    if (this.nativeNode.id) {
-      name = name + "#" + this.nativeNode.id;
+    getAttributes() {
+      if (!this.nativeNode.attributes) {
+        return [];
+      }
+
+      var attributes = [];
+
+      for (var i = 0; i < this.nativeNode.attributes.length; i++) {
+        attributes.push(new XPathDOM(this.nativeNode.attributes[i]));
+      }
+
+      return attributes;
     }
 
-    return "Node<" + name + ">";
-  };
+    getParent() {
+      return new XPathDOM(this.nativeNode.parentNode || this.nativeNode.ownerElement);
+    }
+
+    getOwnerDocument() {
+      if (this.nativeNode.ownerDocument) {
+        return new XPathDOM(this.nativeNode.ownerDocument);
+      }
+    }
+
+    getElementById(id) {
+      var node = this.nativeNode.getElementById(id);
+
+      if (node) {
+        return new XPathDOM(node);
+      }
+    }
+
+    isEqual(node) {
+      return this.getNativeNode() === node.getNativeNode();
+    }
+
+    compareDocumentPosition(other) {
+      var self = this;
+
+      if (self.getNativeNode() instanceof Attr) {
+        self = self.getParent();
+      }
+
+      if (other.getNativeNode() instanceof Attr) {
+        other = other.getParent();
+      }
+
+      var comparing = self.getNativeNode().compareDocumentPosition(other.getNativeNode());
+
+      if (comparing & DocumentPosition.PRECEDING) {
+        return 1;
+      } else if (comparing & DocumentPosition.FOLLOWING) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+
+    toString() {
+      var name;
+
+      if (this.nativeNode.tagName) {
+        name = this.nativeNode.tagName.toLowerCase();
+      } else {
+        name = this.nativeNode.nodeName + "(" + this.nativeNode.nodeValue + ")";
+      }
+
+      if (this.nativeNode.className) {
+        name = name + "." + this.nativeNode.className.split(/\s+/g).join(".");
+      }
+
+      if (this.nativeNode.id) {
+        name = name + "#" + this.nativeNode.id;
+      }
+
+      return "Node<" + name + ">";
+    }
+  }
 
   var evaluator = new XPathEvaluator(XPathDOM);
 
